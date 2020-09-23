6f02d7bd711558907a959424f13fd9645b32e13b
Mathe Sharvadze <sharvadzemat@gmail.com>
2020-09-23 14:17:14 +0400

    first 3 ONLINE ASSESSMENT mock interviews

---
+++ leetcode/num6.js
@@ -0,0 +1,48 @@
+// Combination Sum
+// Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+
+// The same repeated number may be chosen from candidates unlimited number of times.
+
+// Note:
+
+// All numbers (including target) will be positive integers.
+// The solution set must not contain duplicate combinations.
+// Example 1:
+
+// Input: candidates = [2,3,6,7], target = 7,
+// A solution set is:
+// [
+//   [7],
+//   [2,2,3]
+// ]
+// Example 2:
+
+// Input: candidates = [2,3,5], target = 8,
+// A solution set is:
+// [
+//   [2,2,2,2],
+//   [2,3,3],
+//   [3,5]
+// ]
+
+
+// Constraints:
+
+// 1 <= candidates.length <= 30
+// 1 <= candidates[i] <= 200
+// Each element of candidate is unique.
+// 1 <= target <= 500
+
+
+var combinationSum = function (candidates, target) {
+  const ans = []
+  const combination = (prefix, start, sum) => {
+    if (sum === target) ans.push(prefix)
+    for (let i = start; i < candidates.length; i++) {
+      const candidate = candidates[i], total = sum + candidate
+      if (sum <= target) combination(prefix.concat(candidate), i, total)
+    }
+  }
+  combination([], 0, 0)
+  return ans
+};
